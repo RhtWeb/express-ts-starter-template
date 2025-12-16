fa9c871c295ba1046bbeacf8f4a275924ab37aa7
Rohit Singh <55020650+RhtWeb@users.noreply.github.com>
2025-12-17 00:50:48 +0530

    add env config

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,12 @@
+import { Config } from "./config/index.js";
+
 function welcome(name: string) {
   const user = {
     firstname: "Rohit",
   };
 
   const fname = user.firstname;
-
+  console.log(Config.NODE_ENV);
   console.log("welcome" + name + fname);
 }
 
